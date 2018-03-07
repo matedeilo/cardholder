c70ea8eed0f1489b1c291465c3f6037a2b2e6e48
Manuel <mtejeda21@gmail.com>
2018-03-06 20:24:04 -0500

    modify readme

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const CardView = () => {
+    return (
+        <div className="card-view">
+            <label className="number">4444 4444 4444 4444</label>
+            <label className="name">Manuel Tejeda</label>
+            <div className="issuer">Visa</div>
+        </div>
+    )
+}
+
 class App extends Component {
     render() {
-        return <div className="App" />;
+        return (
+            <div className="App">
+                <CardView />
+            </div>
+            )
     }
 }
 
